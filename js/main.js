1c3f6fdafe47d446839aa6a6e0a93fbdafc402bd
JulioRodriguezS <julio.rodriguez.soberano@gmail.com>
2020-08-06 16:19:00 -0500

    actualizando estilos de letra

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load",()=>{
+  console.log("ready")
+})
+
+document.getElementById('download_pdf').addEventListener("click", DownloadPdf(e))
+
+function DownloadPdf(e){
+  e.preventDefault()
+  alert('funcion pendiente')
+}  
